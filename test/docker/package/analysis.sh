74ea213df7768743afdbd40f5aec4c0e3f02535c
Davide Laezza <truzzialrogo@gmx.com>
2018-12-14 00:37:43 +0100

    Fixing jq filter in analysis.sh

--- test/docker/package/analysis.sh
+++ test/docker/package/analysis.sh
@@ -5,13 +5,13 @@ RESULTS_DIR="$HOME/results"
 
 cd "$HOME/haros"
 
-python -m haros analyse -n
-python -m haros export -p massive-analysis "$HOME/.haros/export"
+python -m haros --debug analyse -n
+python -m haros --debug export -p massive-analysis "$HOME/.haros/export"
 
 cd "$HOME/.haros/export/massive-analysis/compliance"
 
-jq "{ \
+jq -n "{ \
     package: \"$PACKAGE\", \
     hash: \"$PACKAGE_HASH\", \
-    queries: inputs[] | select(.comment|startswith(\"Query\")) \
+    queries: [inputs[] | select(.comment|startswith(\"Query\"))] \
 }" runtime/*.json source/*.json > "$RESULTS_DIR/$PACKAGE.json"
