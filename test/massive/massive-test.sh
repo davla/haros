3b06969cbc424b6f5e69db1e4b4db0afc1ba821a
Davide Laezza <truzzialrogo@gmx.com>
2018-12-18 12:57:21 +0100

    massive-tests: refactoring main script

--- test/massive/massive-test.sh
+++ test/massive/massive-test.sh
@@ -1,5 +1,37 @@
 #!/usr/bin/env bash
 
+function rm-images {
+    local PATTERN="$1"
+
+    docker image ls | grep -P "$PATTERN" | grep -v 'haros-deps' \
+        | awk '{print $1 ":" $2}' | xargs docker image rm
+}
+
+function latest-hash {
+    local GIT_REPO="$1"
+    local BRANCH="${2:-master}"
+
+    local TMP_DIR="$(mktemp -d)"
+    git clone --depth 1 "$GIT_REPO" "$TMP_DIR"
+    git -C "$TMP_DIR" checkout "$BRANCH"
+
+    local LATEST_HASH="$(git -C "$TMP_DIR" rev-parse HEAD | cut -c 1-7)"
+
+    rm -rf "$TMP_DIR"
+
+    echo "$LATEST_HASH"
+}
+
+function build-if-not-in-hub {
+    local SERVICE="$1"
+    local FORCE_BUILD="${2:-false}"
+
+    if [[ "$FORCE_BUILD" == 'true' ]] || ! docker-compose pull "$SERVICE"; then
+        docker-compose build "$SERVICE"
+        docker-compose push "$SERVICE"
+    fi
+}
+
 BASE_DIR="$(dirname "${BASH_SOURCE[0]}" | xargs readlink -f)"
 NOW="$(date '+%Y-%m-%d@%H:%M')"
 export RESULTS_DIR="$BASE_DIR/results/$NOW"
@@ -7,6 +39,7 @@ export ROS_DISTRO='melodic'
 
 # Results for this run of the analysis
 mkdir -p "$RESULTS_DIR"
+chmod -R o+w "$RESULTS_DIR"
 
 # Updating rosdistro and logging hash
 git submodule update
@@ -14,10 +47,11 @@ git submodule status | grep rosdistro | xargs \
     | awk '{print substr($1, 1, 7)}' \
     | xargs -i echo "Rosdistro hash: {}" >> "$RESULTS_DIR/main.log"
 
-[[ -d /src/bonsai-code ]] \
-    && BONSAI_SRC='/src/bonsai-code' \
-    || BONSAI_SRC='../../../bonsai'
-export BONSAI_HASH="$(git -C "$BONSAI_SRC" rev-parse HEAD | cut -c 1-7)"
+[[ -d ../../../bonsai ]] \
+    && export BONSAI_HASH="$(git -C "$BONSAI_SRC" rev-parse HEAD \
+        | cut -c 1-7)" \
+    || export BONSAI_HASH="$(latest-hash 'https://github.com/davla/bonsai.git' \
+        'py-parser')"
 export HAROS_HASH="$(git rev-parse HEAD | cut -c 1-7)"
 
 cd ../docker
@@ -30,45 +64,32 @@ cd ../docker
 docker image ls | grep -P '(bonsai|haros)' | grep -v 'haros-deps' \
     | awk '{print $2}' | xargs docker image rm
 
-docker-compose pull base || docker-compose build base
-docker-compose push base
-docker-compose pull haros-deps || docker-compose build haros-deps
-docker-compose push haros-deps
+build-if-not-in-hub 'base'
+build-if-not-in-hub 'haros-deps'
 
-docker-compose pull bonsai || docker-compose build bonsai
-docker-compose push bonsai
-
-docker-compose pull haros || docker-compose build haros
-docker-compose push haros
+build-if-not-in-hub 'bonsai'
+build-if-not-in-hub 'haros'
 
 cd - &> /dev/null
 
 pipenv install
 DISTRIBUTION_FILE="$BASE_DIR/rosdistro/$ROS_DISTRO/distribution.yaml"
 pipenv run python rosdistro.py "$DISTRIBUTION_FILE" --names --urls \
-    | while read PACKAGE URL; do
+    | grep mavros | while read PACKAGE URL; do
         cd ../docker
 
-        git clone --depth 1 "$URL" "$PACKAGE"
-
         export PACKAGE
-        export PACKAGE_HASH="$(git -C "$PACKAGE" rev-parse HEAD | cut -c 1-7)"
+        export PACKAGE_HASH="$(latest-hash "$URL")"
 
-        rm -rf "$PACKAGE"
-
-        docker-compose pull package-build || docker-compose build package-build
-        docker-compose push package-build
-        docker-compose pull analysis || docker-compose build analysis
-        docker-compose push analysis
+        build-if-not-in-hub 'package-build'
+        build-if-not-in-hub 'analysis'
 
         docker-compose up analysis
         docker-compose down
 
-        docker image ls | grep "$PACKAGE" | awk '{print $2}' \
-            | xargs docker image rm
+        rm-images "$PACKAGE"
       done
 
-docker image ls | grep -P '(bonsai|haros)' | grep -v 'haros-deps' \
-    | awk '{print $2}' | xargs docker image rm
+rm-images '(bonsai|haros)'
 
 cd - &> /dev/null
