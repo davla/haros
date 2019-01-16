c2510ca36a4be7be1825a19258f8d750db3eaea2
Davide Laezza <truzzialrogo@gmx.com>
2019-01-16 14:56:06 +0100

    Refactoring massive tests to use release package from rosinstall_generator

--- test/docker/package/analysis.sh
+++ test/docker/package/analysis.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
 source /home/ros/catkin_ws/devel/setup.bash
-RESULTS_DIR="$HOME/results"
 
-cd "$HOME/haros"
+RESULTS_FILE="$HOME/results/${PACKAGE/\//--}.json"
+
+cd "$HOME/haros" || exit 1
 
 python -m haros --debug analyse -n
 python -m haros --debug export -p massive-analysis "$HOME/.haros/export"
 
-cd "$HOME/.haros/export/massive-analysis/compliance"
-
-jq -n "{ \
-    package: \"$PACKAGE\", \
-    hash: \"$PACKAGE_HASH\", \
-    queries: [inputs[] | select(.comment|startswith(\"Query\"))] \
-}" runtime/*.json source/*.json > "$RESULTS_DIR/$PACKAGE.json"
+if cd "$HOME/.haros/export/massive-analysis/compliance" &> /dev/null; then
+    jq -n "{
+        package: \"$PACKAGE\",
+        hash: \"$PACKAGE_HASH\",
+        queries: [inputs[] | select(.comment
+                | type == \"string\" and startswith(\"Query\"))]
+    }" runtime/*.json source/*.json > "$RESULTS_FILE"
+else
+    jq -n "{
+        package: \"$PACKAGE\",
+        hash: \"$PACKAGE_HASH\",
+        queries: []
+    }" > "$RESULTS_FILE"
+fi
