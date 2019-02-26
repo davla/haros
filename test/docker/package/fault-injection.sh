ff657697e4f11d39c676becba6cbf097cbfef19c
Davide Laezza <truzzialrogo@gmx.com>
2019-02-27 00:55:27 +0100

    Adding fault injection script

---
+++ test/docker/package/fault-injection.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+INPUT_FILE="$HOME/results/${PACKAGE/\//--}.json"
+OUTPUT_FILE="$HOME/results/${PACKAGE/\//--}-faulted.json"
+
+bash analysis.sh "$INPUT_FILE"
+
+jq -r ".queries[]
+    | select(.rule | endswith(\"info\"))
+    | .comment[15:-1]
+    | gsub(\",\"; \"\") | gsub(\":\"; \" \")
+    | split(\" \")
+    | select(.[-1] != \"?\")
+    | [.[1], .[2], .[-1]] | join(\" \")" "$INPUT_FILE" \
+    | while read FILE LINE NAME; do
+        FILE="$HOME/catkin_ws/src/$FILE"
+        sed -i "${LINE}s/$NAME/${NAME:1}/" "$FILE"
+    done
+
+bash analysis.sh "$OUTPUT_FILE"
